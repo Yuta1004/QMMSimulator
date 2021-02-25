9b1c7e26bd5c9ad2179a73ec2bfeb71a4e21e21e
NakagamiYuta <guguru0014@gmail.com>
2021-02-26 07:04:57 +0900

    [change] 乱数設定のUIの表示位置と連動機能を変更

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -129,6 +129,8 @@ public class MainUIController implements Initializable {
         xvalRandomC.selectedProperty().addListener((__, oldV, newV) -> {
             xvalL.setDisable(newV);
             xvalC.setDisable(newV);
+            rnumL.setDisable(newV);
+            rnumC.setDisable(newV);
             if(newV) {
                 xInitSettings = XInitSettings.random();
             } else {
